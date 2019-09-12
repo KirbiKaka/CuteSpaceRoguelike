40b7f61de84a4d7b9226f22bd141fc0cd8b4f3fa
Sawyer Gilbert <sawyergilbert@SawyerGilbertNew-MBPR.local>
2019-09-12 14:27:07 -0700

    rover, adventure nod and event manager setup

---
+++ CuteSpace/Assets/Scripts/AdventureNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventureNode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.AddListenerForTestEvent(handleTestEvent);
+    }
+
+    // Update is called once per frame
+    void handleTestEvent(int thing)
+    {
+        print(thing);
+    }
+}
---
+++ CuteSpace/Assets/Scripts/Rover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Rover : MonoBehaviour
+{
+    //Declare vars and const
+    float roverSpeed;
+    float eventLocationX = 8;
+    TestEvent testEvent = new TestEvent();
+    int testNumber = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.AddInvokerForTestEvent(this);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gameObject.transform.position.x < eventLocationX - 2)
+        {
+            transform.Translate(Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            testEvent.Invoke(testNumber);
+        }
+
+    }
+
+    public void TestEventAddedEventListener(UnityAction<int> listener)
+    {
+        testEvent.AddListener(listener);
+    }
+}
