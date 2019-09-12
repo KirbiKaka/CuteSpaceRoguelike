990bba8e1790994849eaefda4d49b9df9815236d
Lukas Nicola <lukas@pocketgems.com>
2019-09-12 13:46:05 -0700

    More main menu stuff

---
+++ CuteSpace/Assets/Scripts/MainMenuUIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+
+    public GameObject shop;
+    public GameObject mainMenu;
+
+    private void Start()
+    {
+        InitializeShop();
+        InitializeMainMenu();
+    }
+
+    private void InitializeMainMenu()
+    {
+        mainMenu.SetActive(true);
+    }
+
+    public void InitializeShop()
+    {
+        shop.SetActive(false);
+    }
+
+    public void DisplayShop()
+    {
+        shop.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void CloseShop()
+    {
+        shop.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
---
+++ CuteSpace/Assets/Scripts/StoreButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreButton : MonoBehaviour
+{
+    public Texture defaultButton;
+    public Texture hoverButton;
+    private RawImage currentButton;
+    private RawImage storeBackground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeImage();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    void InitializeImage()
+    {
+        currentButton = gameObject.GetComponent<RawImage>();
+        currentButton.texture = defaultButton;
+    }
+
+    public void MouseEnter()
+    {
+        currentButton.texture = hoverButton;
+        Debug.Log("Moused Over Button");
+    }
+
+    public void MouseExit()
+    {
+        currentButton.texture = defaultButton;
+        Debug.Log("Mouse Left Button");
+    }
+
+    public void ResetButtons()
+    {
+        currentButton.texture = defaultButton;
+    }
+
+}
