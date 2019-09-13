ef089b5bbadf002cc34375dccaa3e85620fe6cb2
Sawyer Gilbert <sawyergilbert@SawyerGilbertNew-MBPR.local>
2019-09-13 13:07:07 -0700

    added trader encounter

---
+++ CuteSpace/Assets/Scripts/Minigame/Encounters/TraderEncounter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraderEncounter : AdventureNode
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override public int GetResearchOutcome()
+    {
+        switch (GetChoiceMade())
+        {
+            case OUTCOME_1_MAIN:
+                return 40;
+            default:
+                return 0;
+        }
+    }
+
+    override public int GetScrapOutcome()
+    {
+        switch (GetChoiceMade())
+        {
+            case OUTCOME_1_MAIN:
+                return -15;
+            default:
+                return 0;
+        }
+        
+    }
+}
